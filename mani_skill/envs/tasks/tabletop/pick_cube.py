e075d6ea3c3097aa70ac621d1f8f5ee881e1c3a5
hu.po <hugo.p.cmu@gmail.com>
2024-06-10 15:22:53 -0500

    ignore

--- mani_skill/envs/tasks/tabletop/pick_cube.py
+++ mani_skill/envs/tasks/tabletop/pick_cube.py
@@ -28,7 +28,7 @@ class PickCubeEnv(BaseEnv):
     @property
     def _default_sensor_configs(self):
         pose = sapien_utils.look_at(eye=[0.3, 0, 0.6], target=[-0.1, 0, 0.1])
-        return [CameraConfig("base_camera", pose, 128, 128, np.pi / 2, 0.01, 100)]
+        return [CameraConfig("base_camera", pose, 256, 256, np.pi / 2, 0.01, 100)]
 
     @property
     def _default_human_render_camera_configs(self):
